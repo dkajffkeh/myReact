59a446c7f68f911439f8d160a6170cb2db525793
patrickYoo <yhy1045@naver.com>
2022-06-23 22:44:48 +0900

    생활코딩 13 React Developers 수강

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 
@@ -8,12 +7,53 @@ class App extends Component {
 
     return (
       <div className="App">
-        Hello React!
+        <Subject title="WEB" sub="abcd"></Subject>
+        <Navigation></Navigation>
+        <Content abc="abc" haha="haha"></Content>
       </div>
     );
 
   }
 
+}
+
+class Navigation extends Component {
+  render () {
+    return (
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JS</a></li>
+        </ul>
+    </nav>
+    );
+  }
+}
+  
+class Content extends Component {
+
+  render () {
+    return (
+      <article>
+        <h2>{this.props.abc}</h2>
+        {this.props.haha}
+    </article>
+    );
+  }
+
+}
+
+class Subject extends Component {
+
+  render () {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>    
+    );
+  }
 }
 
 export default App;
